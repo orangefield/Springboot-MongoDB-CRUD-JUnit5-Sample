ce7ecb084bd10050e15a1f77accb6d4573dc44f3
field <jsw_777@naver.com>
2022-05-11 10:00:20 +0900

    Initial Project

---
+++ src/main/java/site/orangefield/mongocrudsample/MongoCrudSampleApplication.java
@@ -0,0 +1,13 @@
+package site.orangefield.mongocrudsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoCrudSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoCrudSampleApplication.class, args);
+	}
+
+}
